ecc1ad8d9dac461f8db314dd9176a08d467d7713
Quan Nguyen <71422583+AllenNguyen0417@users.noreply.github.com>
2021-12-12 00:47:40 -0800

    Python_Problem_Solving

---
+++ Designer_PDF_Viewer/main.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'designerPdfViewer' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY h
+#  2. STRING word
+#
+
+def designerPdfViewer(h, word):
+    # Write your code here
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    res = []
+    for i in word:
+        res.append(h[alphabet.index(i)])
+    return max(res) * len(res)
+
+
+if __name__ == '__main__':
+
+    h = list(map(int, input().rstrip().split()))
+
+    word = input()
+
+    result = designerPdfViewer(h, word)
+
+    print(result)
